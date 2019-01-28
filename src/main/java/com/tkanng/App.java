f14ee3f8a9d0f44471b4ee4533585ef0a39ed66f
tkanng <tkanng@pc.lan>
2019-01-28 23:41:43 +0800

    spark app

---
+++ src/main/java/com/tkanng/App.java
@@ -0,0 +1,8 @@
+package com.tkanng;
+import com.tkanng.helloworld.HelloWorld;
+public class App {
+  public static void main(String[] args) {
+    System.out.println(new HelloWorld().sayHello());
+    System.out.println("Hello,World!");
+  }
+}
---
+++ src/test/scala/com/tkanng/H.scala
@@ -0,0 +1,10 @@
+package com.tkanng
+
+import org.scalatest.{BeforeAndAfter, FunSuite, PrivateMethodTester}
+
+class H extends FunSuite {
+    test("An empty Set should have size 0") {
+    assert(Set.empty.isEmpty)
+  }
+}
+
